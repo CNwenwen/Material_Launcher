9c28f004788a6d6a34fd1d2982707eabea07f16f
CNwenwen <1218858474@qq.com>
2024-11-10 11:21:15 +0800

    feature:初始代码

---
+++ WpfApp15/DownloadPage.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using StarLight_Core.Installer;
+using StarLight_Core.Utilities;
+using MaterialDesignThemes.Wpf;
+using StarLight_Core.Enum;
+using StarLight_Core.Models.Downloader;
+using StarLight_Core.Models.Installer;
+
+namespace WpfApp15;
+
+public partial class DownloadPage : Page
+{
+    public DownloadPage()
+    {
+        InitializeComponent();
+        DownloadAPIs.SwitchDownloadSource(DownloadSource.BmclApi);
+        DownloaderConfig.MaxThreads = 128;
+    }
+
+    public class DownloadMCData
+    {
+        public string DisplayId { get; set; }
+        public string Id { get; set; }
+    }
+    
+    public class DownloadFabData
+    {
+        public string DisplayVersion { get; set; }
+        public string Version { get; set; }
+    }
+    private async void DownloadPage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            List<DownloadMCData> downloadDatas = new List<DownloadMCData>();
+            foreach (var downloaddata in await InstallUtil.GetGameCoresAsync())
+            {
+                if (downloaddata.Type == "release")
+                {
+                    downloadDatas.Add(new DownloadMCData()
+                    {
+                        Id = downloaddata.Id,
+                        DisplayId = downloaddata.Id + "，" + "正式版"
+                    });
+                }
+                else if (downloaddata.Type == "snapshot")
+                {
+                    downloadDatas.Add(new DownloadMCData()
+                    {
+                        Id = downloaddata.Id,
+                        DisplayId = downloaddata.Id + "，" + "快照版"
+                    });
+                }
+                else
+                {
+                    downloadDatas.Add(new DownloadMCData()
+                    {
+                        Id = downloaddata.Id,
+                        DisplayId = downloaddata.Id + "，" + "远古版"
+                    });
+                }
+            }
+            CoreVerBox.ItemsSource = downloadDatas;
+            CoreVerBox.DisplayMemberPath = "DisplayId";
+            CoreVerBox.SelectedValuePath = "Id";
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+
+    private async void CoreVerBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        try
+        {
+            List<DownloadFabData> downloadFabDatas = new List<DownloadFabData>();
+            downloadFabDatas.Add(new DownloadFabData()
+            {
+                DisplayVersion = "不下载Fabric",
+                Version = null
+            });
+            foreach (var downloadfabdata in await FabricInstaller.FetchFabricVersionsAsync(CoreVerBox.SelectedValue.ToString()))
+            {
+                downloadFabDatas.Add(new DownloadFabData()
+                {
+                    DisplayVersion = downloadfabdata.Version + "（" + downloadfabdata.GameVersion + "）",
+                    Version = downloadfabdata.Version
+                });
+            }
+
+            FabVerBox.ItemsSource = downloadFabDatas;
+            FabVerBox.DisplayMemberPath = "DisplayVersion";
+            FabVerBox.SelectedValuePath = "Version";
+            FabVerBox.IsEnabled = true;
+            HintAssist.SetHint(FabVerBox,"请选择Fabric版本");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+
+    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            MinecraftInstaller installer = new MinecraftInstaller(CoreVerBox.SelectedValue.ToString(), ".minecraft");
+            installer.OnProgressChanged += (status, progress) =>
+            {
+                ProgressBar.Value = progress;
+                TextBlock.Text = status;
+            };
+            installer.OnSpeedChanged += (speed) =>
+            {
+                Console.WriteLine($"{speed}/s");
+            };
+            CancellationTokenSource source = new();
+            CancellationToken token = source.Token;
+            await installer.InstallAsync(null,true,token);
+            if (FabVerBox.SelectedValue != null)
+            {
+                FabricInstaller installerfab = new FabricInstaller(CoreVerBox.SelectedValue.ToString(),
+                    FabVerBox.SelectedValue.ToString(),
+                    default);
+                installerfab.OnProgressChanged += (status, progress) =>
+                {
+                    ProgressBar.Value = progress;
+                    TextBlock.Text = status;
+                };
+                installerfab.OnSpeedChanged += (speed) =>
+                {
+                    Console.WriteLine($"{speed}/s");
+                };
+                await installerfab.InstallAsync();
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+}
---
+++ WpfApp15/StartPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+using StarLight_Core.Authentication;
+using StarLight_Core.Enum;
+using StarLight_Core.Launch;
+using StarLight_Core.Models.Authentication;
+using StarLight_Core.Models.Launch;
+using StarLight_Core.Utilities;
+
+namespace WpfApp15;
+
+public partial class StartPage : Page
+{
+    public StartPage()
+    {
+        InitializeComponent();
+        GetJavas();
+        GetGameCores();
+    }
+
+    public class Item
+    {
+        public string Id { get; set; }
+    }
+    private void GetGameCores()
+    {
+        List<Item> items = new List<Item>();
+        foreach (var item in GameCoreUtil.GetGameCores(".minecraft"))
+        {
+            if (item.Exception == null)
+            {
+                items.Add(new Item 
+                    { Id = item.Id,
+                    });
+            }
+            else
+            {
+                items.Add(new Item 
+                { Id = $"错误的版本：{item.Id}"
+                });
+            }
+        }
+        CoreBox.ItemsSource = items;
+    }
+
+
+    public class Item2
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+    private void GetJavas()
+    {
+        List<Item2> items = new List<Item2>();
+        //items.Add(new Item2
+        //{
+            //Name = "让SMCL自动选择Java",
+            //Path = ""
+        //});
+        foreach (var item in JavaUtil.GetJavas())
+        {
+            items.Add(new Item2
+            {
+                Name = $"Java {item.JavaSlugVersion}, {item.JavaLibraryPath}",
+                Path = item.JavaPath
+            });
+        }
+        JavaBox.ItemsSource = items;
+    }
+
+    private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        StartButton.IsEnabled = true;
+    }
+
+    private async void StartButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            BaseAccount account;
+            account = new OfflineAuthentication(NameBox.Text).OfflineAuth();
+            LaunchConfig args = new() // 配置启动参数
+            {
+                Account = new()
+                {
+                    BaseAccount = account // 账户
+                },
+                GameCoreConfig = new()
+                {
+                    Root = ".minecraft", // 游戏根目录(可以是绝对的也可以是相对的,自动判断)
+                    Version = CoreBox.Text, // 启动的版本
+                    IsVersionIsolation = true, //版本隔离
+                    //Nide8authPath = ".minecraft\\nide8auth.jar", // 只有统一通行证需要
+                    //UnifiedPassServerId = "xxxxxxxxxxxxxxxxxx" // 同上
+                },
+                JavaConfig = new()
+                {
+                    JavaPath = JavaBox.SelectedValue.ToString(), // Java 路径(绝对路径)
+                    MaxMemory = 16384,
+                    MinMemory = 1000
+                },
+                GameWindowConfig = new()
+                {
+                    Width = 854,
+                    Height = 480,
+                    IsFullScreen = false
+                }
+            };
+            var launch = new MinecraftLauncher(args); // 实例化启动器
+            var la = await launch.LaunchAsync(ReportProgress); // 启动
+                
+// 日志输出
+            la.ErrorReceived += (output) => Console.WriteLine($"{output}");
+            la.OutputReceived += (output) => Console.WriteLine($"{output}");
+                
+            if (la.Status == Status.Succeeded)
+            {
+                SnackbarOne.IsActive = true;
+                await Task.Delay(1000);
+                SnackbarOne.IsActive = false;
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+    }
+
+    private void ReportProgress(ProgressReport obj)
+    {
+        ProgressBar.Value = obj.Percentage;
+    }
+
+    private void NameBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        StartButton.IsEnabled = true;
+    }
+}
